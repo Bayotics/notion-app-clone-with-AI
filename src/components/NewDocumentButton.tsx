82f6c851acab84f42e17864f68927a08009a71ef
Abdullahi Shobaloju <sabdullahi@cinnsol.com>
2025-03-01 02:02:32 +0100

    firebase db init and populated successfully

--- src/components/NewDocumentButton.tsx
+++ src/components/NewDocumentButton.tsx
@@ -1,8 +1,24 @@
+'use client'
+import { useTransition } from "react";
 import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
+import { createNewDocument } from "../../actions/actions";
 
 function NewDocumentButton () {
+    const [isPending, startTransition] = useTransition()
+    const router = useRouter();
+    const handleCreateNewDocument = () => {
+        startTransition(async () => {
+            const {docId} = await(createNewDocument());
+            router.push(`/doc/${docId}`);
+        })
+    }
     return (
-        <div><Button>New Document</Button></div>
+        <div>
+            <Button onClick={handleCreateNewDocument} disabled = {isPending}>
+                {isPending ? 'Creating...' : 'New Document'}
+            </Button>
+        </div>
     )
 }
 
